9ed66ab9dc20f337538896213084bfe6f99fc1a8
TheEgid <egid.f1@gmail.com>
2019-09-15 11:30:45 +0300

    Fix whitespaces

--- lesson5_step6.py
+++ lesson5_step6.py
@@ -6,17 +6,14 @@ import time
 
 link = "http://suninjuly.github.io/huge_form.html"
 browser = webdriver.Chrome()
+
 try:
-
     browser.get(link)
-
     elements = browser.find_elements(By.CSS_SELECTOR, "div.first_block input")
     for element in elements:
         element.send_keys("Мой ответ")
-
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
-
     time.sleep(15)
 
 except Exception as error:
